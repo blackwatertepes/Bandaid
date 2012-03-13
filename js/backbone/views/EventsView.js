3d1795b22dbebf5ab87a11e6ffd9ba86bfced18e
Ty <tyler@wargoats.com>
2012-03-13 16:41:52 -0700

    added bootstrap

--- js/backbone/views/EventsView.js
+++ js/backbone/views/EventsView.js
@@ -9,7 +9,7 @@ var EventsView = Backbone.View.extend({
   },
 
   render: function() {
-    $('#events', this.el).append("<span id='showing'>Showing concerts for <span id='date'></span> <span id='change'>Change Date</span></span>");
+    $('#events', this.el).append("<div id='header'><span id='showing'>Showing concerts for <span id='date'></span></span> <button id='change' class='btn btn-mini'><i class='icon-calendar'></i> Change Date</button></div>");
   },
 
   updateDate: function(date) {
@@ -19,7 +19,7 @@ var EventsView = Backbone.View.extend({
   addItem: function(time, name, place, event_url, map_url, event_id, lat, lon) {
     var letter = Alphabet.getLetter(this.counter).toUpperCase();
     var marker = Map.addMarker({latitude: lat, longitude: lon, letter: letter});
-    $('#events', this.el).append("<div class='event'><div class='info'><span class='letter'>"+letter+"</span> "+time+"<br/><a href='"+event_url+"'>"+name+"</a><br/><a href='"+map_url+"'>"+place+"</a></div><div class='bitebrew'><a class='bite' onclick='showBite("+marker.__gm_id+", "+event_id+")' href='#'>Get a Bite</a><a class='brew' onclick='showBrew("+marker.__gm_id+", "+event_id+")' href='#'>Get a Brew</a></div></div>");
+    $('#events', this.el).append("<div class='event'><span class='label'>"+letter+"</span><div class='info'> "+time+"<br/><a href='"+event_url+"'>"+name+"</a><br/><a href='"+map_url+"'>"+place+"</a></div><div class='bitebrew'><a class='bite' onclick='showBite("+marker.__gm_id+", "+event_id+")' href='#'>Get a Bite</a><a class='brew' onclick='showBrew("+marker.__gm_id+", "+event_id+")' href='#'>Get a Brew</a></div></div>");
     this.counter++;
   },
 
